edf7ee2bb2c5fa933249702193452bbc2f1f5de9
渡辺　泰平 <wattaihei.rapyuta@gmail.com>
2019-10-01 17:48:31 +0900

    1001 some bug in Sudoku removed

--- sudoku/views.py
+++ sudoku/views.py
@@ -4,8 +4,7 @@ from .models import Sudoku
 from .forms import SudokuForm
 import datetime
 
-
-
+# 基本画面
 def home(request):
     if (request.method == 'POST'):
         sudoku_set = sudoku_make.create()
@@ -27,7 +26,7 @@ def home(request):
     }
     return render(request, 'sudoku/home.html', params)
 
-
+# 削除するか確かめる
 def check_delete(request):
     password = 'isaka'
     params = {'title': 'Confirmation'}
@@ -43,7 +42,7 @@ def check_delete(request):
     return render(request, 'sudoku/checkdelete.html', params)
 
 
-
+# 削除
 def delete(request):
     data = Sudoku.objects.all()
     if (request.method == 'POST'):
@@ -53,34 +52,37 @@ def delete(request):
     return render(request, 'sudoku/delete.html', param)
 
 
-
+# 問題を表から取ってきて表示
 def question(request, num):
     Q_d = Sudoku.objects.get(id=num).question
     Question = [[int(num) for num in list(row_s)] for row_s in Q_d.split(',')]
     if (request.method == 'POST'):
         nums = request.POST.getlist('num')
         ind = 0
-        #print(nums)
+        print(nums)
         for r in range(9):
             for c in range(9):
                 if Question[r][c] == 0:
                     Question[r][c] = int(nums[ind])
                     ind += 1
         return answer(request, num, Question)
+
     params = {
             'title': 'Question' + str(num),
             'msg':'問題です。一応人間が解けるような難易度ではあるはず。',
             'goto':'answer',
             'num': num,
+            'next_num': num+1,
             'form': SudokuForm(),
             'q': True,
             'next': 'answer',
             'state':Question,
-            'not_max': True
+            'not_max': num+1 < Sudoku.objects.all().last().id,
             }
     return render(request, 'sudoku/index.html', params)
 
 
+# 回答表示
 def answer(request, num, state):
     A_d = Sudoku.objects.get(id=num).answer
     Answer = [[int(num) for num in list(row_s)] for row_s in A_d.split(',')]
@@ -94,7 +96,8 @@ def answer(request, num, state):
             'title': 'Answer' + str(num),
             'q': False,
             'state':Answer,
-            'not_max': not_Max
+            'not_max': not_Max,
+            'num': num,
         }
     if judge is False:
         params['msg'] = '間違いです！残念でした〜'
@@ -102,6 +105,6 @@ def answer(request, num, state):
         params['msg'] = '正解です！すごい！天才！！'
     if not_Max:
         params['goto'] = 'question'
-        params['num'] = num + 1
+        params['next_num'] = num + 1
         params['next'] = 'next question'
     return render(request, 'sudoku/index.html', params)
